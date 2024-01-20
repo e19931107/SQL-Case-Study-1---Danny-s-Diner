255852f7747fe9c038a831336533208456fbec90
Edward <e19931107@gmail.com>
2024-01-20 20:33:14 +0800

    Update Query.sql

--- Query.sql
+++ Query.sql
@@ -68,22 +68,22 @@ ORDER BY order_times DESC;
 
 
 -- 5. Which item was the most popular for each customer?
-    WITH hot_selling_product_for_each_customer AS
-    (
-       SELECT 
-          s.customer_id, 
-          m.product_name, 
-          COUNT(m.product_id) AS order_count,
-          DENSE_RANK() OVER(PARTITION BY s.customer_id ORDER BY COUNT(s.customer_id) DESC) AS `rank`
-       FROM dannys_diner.menu AS m
-       LEFT JOIN dannys_diner.sales AS s
-          ON m.product_id = s.product_id
-       GROUP BY s.customer_id, m.product_name
-    )
-    
-    SELECT customer_id, product_name, order_count
-    FROM hot_selling_product_for_each_customer
-    WHERE `rank` = 1;
+WITH hot_selling_product_for_each_customer AS
+(
+   SELECT 
+      s.customer_id, 
+      m.product_name, 
+      COUNT(m.product_id) AS order_count,
+      DENSE_RANK() OVER(PARTITION BY s.customer_id ORDER BY COUNT(s.customer_id) DESC) AS `rank`
+   FROM dannys_diner.menu AS m
+   LEFT JOIN dannys_diner.sales AS s
+      ON m.product_id = s.product_id
+   GROUP BY s.customer_id, m.product_name
+)
+
+SELECT customer_id, product_name, order_count
+FROM hot_selling_product_for_each_customer
+WHERE `rank` = 1;
 
 | customer_id | product_name | order_count |
 | ----------- | ------------ | ----------- |
