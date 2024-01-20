da4f4aa73b55cbf0abfcf50f91beacc73e0da22b
Edward <e19931107@gmail.com>
2024-01-20 19:44:38 +0800

    Update question 3

--- Query.sql
+++ Query.sql
@@ -1,5 +1,4 @@
 -- 1. What is the total amount each customer spent at the restaurant?
-
 SELECT s.customer_id,
 SUM(f.price) AS total_amount
 FROM dannys_diner.sales AS s
@@ -17,7 +16,6 @@ ORDER BY s.customer_id;
 
 
 -- 2. How many days has each customer visited the restaurant?
-
 SELECT s.customer_id,
 COUNT(DISTINCT s.order_date) AS count_of_customer_visit
 FROM dannys_diner.sales AS s
@@ -32,13 +30,29 @@ ORDER BY s.customer_id;
 | C           | 2                       |
 
 
-
-
 -- 3. What was the first item from the menu purchased by each customer?
+SELECT s.customer_id,
+s.order_date,
+s.product_id,
+m.product_name
+FROM dannys_diner.sales as s
+FULL JOIN dannys_diner.menu as m
+ON s.product_id = m.product_id
+WHERE order_date = (SELECT MIN(order_date) FROM dannys_diner.sales);
 
+-- Result:
+| customer_id | order_date               | product_id | product_name |
+| ----------- | ------------------------ | ---------- | ------------ |
+| A           | 2021-01-01T00:00:00.000Z | 1          | sushi        |
+| B           | 2021-01-01T00:00:00.000Z | 2          | curry        |
+| A           | 2021-01-01T00:00:00.000Z | 2          | curry        |
+| C           | 2021-01-01T00:00:00.000Z | 3          | ramen        |
+| C           | 2021-01-01T00:00:00.000Z | 3          | ramen        |
 
 
 -- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
+
+
 -- 5. Which item was the most popular for each customer?
 -- 6. Which item was purchased first by the customer after they became a member?
 -- 7. Which item was purchased just before the customer became a member?
